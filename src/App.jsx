595934ac91322bce03ab4e46ae50150b2b6c5831
damian <damianl32cooking@gmail.com>
2026-02-08 02:02:01 -0300

    sport subido

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Importación limpia
 import TiendaDeportiva from './pages/TiendaDeportiva';
 import Admin from './pages/Admin';
 
 export default function App() {
   return (
-   <BrowserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<TiendaDeportiva />} />
         <Route path="/admin" element={<Admin />} /> 
-        {/* Si esta línea no está, /admin nunca va a cargar */}
       </Routes>
     </BrowserRouter>
   );
